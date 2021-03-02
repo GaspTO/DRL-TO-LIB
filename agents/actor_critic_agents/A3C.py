I still have to fix the value critic being in the softmax in the baselines

agents/actor_critic_agents/A3C.py
@@ -208,7 +208,7 @@ class Actor_Critic_Worker(torch.multiprocessing.Process,Base_Agent):
         critic_output = model_output[:, -1]
 
         if(self.action_mask_required == True): #todo can't use the forward for this mask cause... critic_output
-            mask = self.get_action_mask()
+            mask = self.get_action_mask() #todo I am not detaching
             unormed_action_values =  actor_output.mul(mask)
             actor_output =  unormed_action_values/unormed_action_values.sum()
         else:

agents/policy_gradient_agents/REINFORCE_Baseline.py
@@ -1,3 +1,4 @@
+from agents.policy_gradient_agents.REINFORCE import Config_Reinforce, REINFORCE
 import numpy as np
 import torch
 import torch.optim as optim
@@ -5,40 +6,24 @@ from torch.distributions import Categorical
 from agents.Base_Agent import Base_Agent, Config_Base_Agent
 
 
-class Config_Reinforce(Config_Base_Agent):
+class Config_Reinforce_Baseline(Config_Reinforce):
     def __init__(self,config=None):
-        Config_Base_Agent.__init__(self,config)
-        if(isinstance(config,Config_Reinforce)):
-            self.discount_rate = config.get_discount_rate()
-            self.learning_rate = config.get_learning_rate()
-        else:
-            self.discount_rate = 0.99
-            self.learning_rate = 1
-    
-    def get_discount_rate(self):
-        if(self.discount_rate == None):
-            raise ValueError("Discount Rate Not Defined")
-        return self.discount_rate
-
-    def get_learning_rate(self):
-        if(self.learning_rate == None):
-            raise ValueError("Learning Rate Not Defined")
-        return self.learning_rate
+        Config_Reinforce.__init__(self,config)
+        
 
 
-class REINFORCE(Base_Agent):
-    agent_name = "REINFORCE"
+class REINFORCE_Baseline(REINFORCE):
+    agent_name = "REINFORCE_Baseline"
     def __init__(self, config):
-        Base_Agent.__init__(self, config)
-        self.policy = self.create_NN_through_NNbuilder(input_dim=self.input_shape, output_size=self.action_size,smoothing=0.001)
+        REINFORCE.__init__(self, config)
+        self.policy = self.create_NN_through_NNbuilder(input_dim=self.input_shape, output_size=self.action_size + 1,smoothing=0.001)
         self.optimizer = optim.Adam(self.policy.parameters(), lr=self.config.get_learning_rate())
-        self.episode_rewards = []
-        self.episode_log_probabilities = []
-
+        
     def reset_game(self):
         """Resets the game information so we are ready to play a new episode"""
         super().reset_game()
         self.episode_log_probabilities = []
+        self.episode_critic_values = []
         self.episode_step_number = 0
 
     def step(self):
@@ -53,24 +38,33 @@ class REINFORCE(Base_Agent):
             self.episode_step_number += 1
         self.episode_number += 1
 
+    """ Network -> Environment """
     def pick_and_conduct_action_and_save_log_probabilities(self):
         """Picks and then conducts actions. Then saves the log probabilities of the actions it conducted to be used for
         learning later"""
-        action, log_probabilities = self.pick_action_and_get_log_probabilities()
+        action, log_probabilities, critic_value = self.pick_action_and_get_log_probabilities_and_critic_value()
         self.store_log_probabilities(log_probabilities)
+        self.store_critic_value(critic_value)
         self.store_action(action)
         self.conduct_action(action)
 
-    def pick_action_and_get_log_probabilities(self):
+    def pick_action_and_get_log_probabilities_and_critic_value(self):
         """Picks actions and then calculates the log probabilities of the actions it picked given the policy"""
-        # PyTorch only accepts mini-batches and not individual observations so we have to add
-        # a "fake" dimension to our observation using unsqueeze
         state = torch.from_numpy(self.state).float().unsqueeze(0).to(self.device)
-        action_values = self.policy.forward(state,self.get_action_mask()).cpu() 
+        model_output = self.policy.forward(state).cpu() #todo questionable
+        action_values = model_output[:, list(range(self.action_size))] #we only use first set of columns to decide action, last column is state-value
+        critic_value = model_output[:, -1]
+        print(critic_value)
         action_values_copy = action_values.detach()
+        if(self.action_mask_required == True): #todo can't use the forward for this mask cause... critic_output
+            mask = self.get_action_mask()
+            unormed_action_values_copy =  action_values_copy.mul(mask)
+            action_values_copy =  unormed_action_values_copy/unormed_action_values_copy.sum()
+        
+
         action_distribution = Categorical(action_values_copy) # this creates a distribution to sample from
         action = action_distribution.sample()
-        return action.item(), torch.log(action_values[0][action])
+        return action.item(), torch.log(action_values[0][action]), critic_value
 
     
     """ Learn """
@@ -80,8 +74,10 @@ class REINFORCE(Base_Agent):
         policy_loss = self.calculate_policy_loss_on_episode(total_discounted_reward)
         self.optimizer.zero_grad()
         policy_loss.backward()
+        print("grad 81->" + str(self.policy.model[6].weight.grad[81]))
         self.optimizer.step()
-        print("...")
+        
+        print("policy_loss = " + str(policy_loss.item()))
 
     def time_to_learn(self):
         """Tells us whether it is time for the algorithm to learn. With REINFORCE we only learn at the end of every
@@ -90,6 +86,37 @@ class REINFORCE(Base_Agent):
 
     
     """ Calculate Loss """
+
+    def calculate_policy_loss_on_episode(self, total_discounted_reward):
+        all_discounted_returns = torch.tensor(self.calculate_discounted_returns())
+        all_critic_values = torch.cat(self.episode_critic_values)
+
+        advantages = all_discounted_returns - all_critic_values 
+        advantages = advantages.detach()
+
+        critic_loss = (all_discounted_returns - all_critic_values)**2
+        critic_loss = critic_loss.mean()
+
+        action_log_probabilities_for_all_episodes = torch.cat(self.episode_log_probabilities)
+        actor_loss = -1.0 * action_log_probabilities_for_all_episodes * advantages
+        actor_loss = actor_loss.mean()
+        
+        total_loss = actor_loss + critic_loss
+        return total_loss
+
+
+
+        #return (-1.0 * torch.tensor(self.calculate_discounted_returns()) * torch.cat(self.episode_log_probabilities)).mean()
+        """Calculates the loss from an episode"""
+        """
+        policy_loss = []
+        for log_prob in self.episode_log_probabilities:
+            policy_loss.append(-log_prob * total_discounted_reward)
+        policy_loss = torch.cat(policy_loss).sum() # We need to add up the losses across the mini-batch to get 1 overall loss
+        return policy_loss
+        """
+
+        
     def calculate_discounted_returns(self):
         discounted_returns = []
         discounted_reward = 0
@@ -98,29 +125,28 @@ class REINFORCE(Base_Agent):
             discounted_returns.insert(0,discounted_reward)
         return discounted_returns
 
-    def calculate_episode_discounted_reward(self):
+    def calculate_episode_discounted_reward(self): #this sucks
         """Calculates the cumulative discounted return for the episode"""
         discounts = self.config.get_discount_rate() ** np.arange(len(self.episode_rewards))
         total_discounted_reward = np.dot(discounts, self.episode_rewards)
         return total_discounted_reward
 
-    def calculate_policy_loss_on_episode(self, total_discounted_reward):
-
-        #return (-1.0 * torch.tensor(self.calculate_discounted_returns()) * torch.cat(self.episode_log_probabilities)).mean()
-        """Calculates the loss from an episode"""
-        
-        policy_loss = []
-        for log_prob in self.episode_log_probabilities:
-            policy_loss.append(-log_prob * total_discounted_reward)
-        policy_loss = torch.cat(policy_loss).sum() # We need to add up the losses across the mini-batch to get 1 overall loss
-        return policy_loss
-        
+    def calculate_critic_loss_and_advantages(self, all_discounted_returns):
+        """Calculates the critic's loss and the advantages"""
+        critic_values = torch.cat(self.critic_outputs)
+        advantages = torch.Tensor(all_discounted_returns) - critic_values
+        advantages = advantages.detach()
+        critic_loss =  (torch.Tensor(all_discounted_returns) - critic_values)**2
+        critic_loss = critic_loss.mean()
 
 
     """ Storage """
     def store_log_probabilities(self, log_probabilities):
         """Stores the log probabilities of picked actions to be used for learning later"""
         self.episode_log_probabilities.append(log_probabilities)
+
+    def store_critic_value(self,critic_value):
+        self.episode_critic_values.append(critic_value)
 
     def store_action(self, action):
         """Stores the action picked"""

utilities/NNbuilder.py
@@ -43,8 +43,9 @@ class NNbuilder(nn.Module):
 
 
     def add_to_model(self,unit,new_dim = None,secondary_operation=None):
+        ''' Secondary operations are operations that do not go into the computation graph, like reshaping '''
         self.model.append(unit)
-        self.secondary_operations.append(secondary_operation)
+        self.secondary_operations.append(secondary_operation) 
         if(new_dim is not None):
             self.last_dim = new_dim
 
@@ -52,7 +53,8 @@ class NNbuilder(nn.Module):
         secondary_op = None
         if(len(self.last_dim) != 2):
             secondary_op = [lambda x: x.reshape(x.size(0),-1)]
-        self.add_to_model(nn.Linear(torch.prod(self.last_dim[1:]).item(),hidden_nodes),\
+        self.add_to_model(\
+            nn.Linear(torch.prod(self.last_dim[1:]).item(),hidden_nodes),\
             torch.tensor([self.last_dim[0],hidden_nodes]),secondary_op)
         if(activation is not None):
             self.add_to_model(self.activations[activation](**act_arguments))       
@@ -60,12 +62,15 @@ class NNbuilder(nn.Module):
     #def add_Conv1d_layer(self,out_channels,kernel_size,stride, activation_function):
 
     def add_Conv2d_layer(self,out_channels,kernel_size,stride,activation: str = None):
+        secondary_op = None
         if(len(self.last_dim)!=4):
-            raise ValueError("Conv needs a 4-D input")
+            if(len(self.last_dim)==3):
+                secondary_op = [lambda x: x.reshape(x.size(0),1,x.size(1),x.size(2))]
+                self.last_dim = torch.tensor([self.last_dim[0],1,self.last_dim[1],self.last_dim[2]])
         
         h = NNbuilder.conv_size_out(self.last_dim[2],kernel_size,stride)
         w = NNbuilder.conv_size_out(self.last_dim[3],kernel_size,stride)
-        self.add_to_model(nn.Conv2d(self.last_dim[1],out_channels,kernel_size,stride),torch.tensor([self.last_dim[0],out_channels,h,w]))
+        self.add_to_model(nn.Conv2d(self.last_dim[1],out_channels,kernel_size,stride),torch.tensor([self.last_dim[0],out_channels,h,w]),secondary_op)
         if(activation is not None):
             self.add_to_model(self.activations[activation]())
 
@@ -84,6 +89,7 @@ class NNbuilder(nn.Module):
         
         x = x + self.smoothing
         if(mask != None):
+            raise ValueError("I'm masking where I shouldn't")
             x = x.mul(mask)
         x = x/x.sum()
         return x
@@ -95,7 +101,7 @@ class NNbuilder(nn.Module):
 
 
 
-'''
+
 config = {
     "input_dim": (1,2,20,20),
     "architecture": (("Conv2d",2,2,1,"ReLU"),
@@ -104,12 +110,19 @@ config = {
         ("Linear",15,"ReLU"),
         ("Linear",1,"ReLU"))
 }    
+input_dim =  (1,2,20,20)
+architecture = (("Conv2d",2,2,1,"ReLU"),
+        ("Conv2d",5,2,1,"ReLU"),
+        ("Linear",5,"ReLU"),
+        ("Linear",15,"ReLU"),
+        ("Linear",1,"ReLU"))
+output_dim
+
 inpute = torch.tensor([1,2,20,20])
 #a = NNbuilder(input_dim=inpute)
 a = NNbuilder(config=config)
 
 print(a(torch.rand(tuple(inpute))))
-'''
 
 
 